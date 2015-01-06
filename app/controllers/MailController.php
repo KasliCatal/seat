9934fa289f9c1a602971c23cc684c791ac62730a
Asher Schaffer <asher@squaretrade.com>
2015-01-06 10:33:19 -0800

    allow recruiters to view all mail

--- app/controllers/MailController.php
+++ app/controllers/MailController.php
@@ -114,7 +114,7 @@ class MailController extends BaseController
             ->lists('characterID');
 
         // Ensure that the current user is allowed to view the mail
-        if (!\Auth::isSuperUser() ) {
+        if (!\Auth::hasAccess('recruiter') ) {
 
             // Get all the keys that have this mail recorded
             $keys_with_mail = DB::table('character_mailmessages')
