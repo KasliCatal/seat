ac7df5e35346ae851d42be3ddfe1076cffa68ce9
KasliCatal <t-stew@hotmail.com>
2015-01-09 19:48:12 -0500

    Create listalttracking.blade.php

---
+++ app/views/corporation/alttracking/listalttracking.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.masterLayout')
+
+@section('html_title', 'All Corporations Alts Tracking')
+
+@section('page_content')
+
+  @foreach ($corporations as $corp)
+
+    <div class="col-md-4">
+      <div class="small-box bg-blue">
+        <div class="inner">
+          <h3>{{ $corp->corporationName }}</h3>
+          <p>From character: {{ $corp->characterName }}</p>
+        </div>
+        <div class="icon">
+          <img src="{{ App\Services\Helpers\Helpers::generateEveImage($corp->corporationID, 32) }}" class="img-circle" />
+        </div>
+        <a href="{{ action('CorporationController@getAltsTracking', array('corporationID' => $corp->corporationID)) }}" class="small-box-footer">
+          View Alts Tracking <i class="fa fa-arrow-circle-right"></i>
+        </a>
+      </div>
+    </div>
+
+  @endforeach
+
+@stop
