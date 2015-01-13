704967fccac09238281e1982b569bdf99e0cf51f
KasliCatal <t-stew@hotmail.com>
2015-01-13 10:35:22 -0500

    Update alttracking.blade.php

--- app/views/corporation/alttracking/alttracking.blade.php
+++ app/views/corporation/alttracking/alttracking.blade.php
@@ -14,7 +14,7 @@
         </div>
         <div class="panel-body">
 
-          <table class="table table-condensed compact table-hover" id="datatable">
+          <table class="table table-condensed compact table-hover" id="altstable">
             <thead>
               <th>Name</th>
               <th>People Group</th>
@@ -50,4 +50,64 @@
     </div> <!-- col-md-12 -->
   </div> <!-- row -->
 
+@stop
+
+@section('javascript')
+<script type="text/javascript" id="js">
+  // Allows modal to be repopulated with data
+  $('body').on('hidden.bs.modal', '.modal', function () {
+    $(this).removeData('bs.modal');
+  });
+
+  // settings for this specific data table
+  $(document).ready(function() {
+      $('#altstable').dataTable( {
+          "searching":   false,
+          "lengthMenu": [[25, 50, 100, -1], [ 25, 50, 100, "All"]],
+          "order":       []
+      } );
+  } );
+
+  // Events to be triggered when the ajax calls have compelted.
+  $( document ).ajaxComplete(function() {
+
+    // Update any outstanding id-to-name fields
+    var items = [];
+    var arrays = [], size = 250;
+
+    $('[rel="id-to-name"]').each( function(){
+      //add item to array
+      if ($.isNumeric($(this).text())) {
+        items.push( $(this).text() );
+      }
+    });
+
+    var items = $.unique( items );
+
+    while (items.length > 0)
+      arrays.push(items.splice(0, size));
+
+    $.each(arrays, function( index, value ) {
+
+      $.ajax({
+        type: 'POST',
+        url: "{{ action('HelperController@postResolveNames') }}",
+        data: {
+          'ids': value.join(',')
+        },
+        success: function(result){
+          $.each(result, function(id, name) {
+
+            $("span:contains('" + id + "')").html(name);
+          })
+        },
+        error: function(xhr, textStatus, errorThrown){
+          console.log(xhr);
+          console.log(textStatus);
+          console.log(errorThrown);
+        }
+      });
+    });
+  });
+</script>
 @stop
